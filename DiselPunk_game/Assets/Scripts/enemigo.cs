8606456ec4254d5a3be72af547e570b1660d45c9
WIN7-PC\Santiago <sanjohn117@gmail.com>
2018-02-19 20:28:07 -0500

    avance1

---
+++ DiselPunk_game/Assets/Scripts/enemigo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemigo : MonoBehaviour {
+
+	public float velocidad;
+	int vida;
+	public float posicion;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+		float posicion = 0.0f;
+		posicion = new Vector2 (0, 0, 0).x.velocidad;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		
+	}
+}
