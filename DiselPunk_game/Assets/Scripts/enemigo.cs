1a66c3b5088ac9738646e4873707b60f18dbd421
WIN7-PC\Santiago <sanjohn117@gmail.com>
2018-02-25 19:22:11 -0500

    movimiento enemigo

--- DiselPunk_game/Assets/Scripts/enemigo.cs
+++ DiselPunk_game/Assets/Scripts/enemigo.cs
@@ -4,24 +4,45 @@ using UnityEngine;
 
 public class enemigo : MonoBehaviour {
 
-	public float velocidad;
-	int vida;
-	public float posicion;
+	public float velocidadH;
+	public float velocidadV;
+	public float vida= 0.0f;
+	float TimerX = 0.0f;
+	float TimerY = 0.0f;
 
 
 	// Use this for initialization
 	void Start () 
 	{
-		
-		float posicion = 0.0f;
-		posicion = new Vector2 (0, 0, 0).x.velocidad;
+
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-
-
+	void Update ()
+	{
 		
-	}
+			transform.position += new Vector3 (Time.deltaTime * velocidadH, 0, 0);
+			TimerX += Time.deltaTime;
+				
+			if (TimerX > 1.0f) 
+			{
+				velocidadH *= -1;
+				TimerX = 0;
+			}
+
+	
+	
+	
+			transform.position += new Vector3 (0,Time.deltaTime*velocidadV,0);
+			TimerY += Time.deltaTime;
+		
+			if (TimerY > 1.0f) 
+			{
+				velocidadV *= -1;
+				TimerY = 0;
+			}
+	
+
+}
 }
